f3dc063cdfe746724aafdb1515b139a25d645cc0
Filip Jerga <jerga99@gmail.com>
2024-08-31 13:54:43 +0200

    Part 10 - Random Movement

--- Assets/Runner_Game/Scripts/Obstacle.cs
+++ Assets/Runner_Game/Scripts/Obstacle.cs
@@ -7,10 +7,38 @@ public class Obstacle : MonoBehaviour
     private int m_Damage;
     [SerializeField]
     private float m_MovementSpeed = 5.0f;
+    [SerializeField]
+    private float m_LineChangeInterval = 2.0f;
+    private float m_LineChangeTimer = 0f;
+    private Vector3 m_TargetPosition;
 
     public int Damage => m_Damage;
 
     void Update() {
-        transform.position += m_MovementSpeed * Time.deltaTime * Vector3.back;
+        if (Time.time > m_LineChangeTimer) {
+            var randomValue = Random.Range(0, 2.0f);
+            m_LineChangeTimer = Time.time + m_LineChangeInterval + randomValue;
+            SelectNewLine();
+        }
+        var speed = m_MovementSpeed * Time.deltaTime;
+        transform.position += speed * Vector3.back;
+
+        var targetPosition = new Vector3(
+            m_TargetPosition.x,
+            m_TargetPosition.y,
+            transform.position.z
+        );
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed / 5);
+    }
+
+    private void SelectNewLine() {
+        var xPositions = new float[] { -3.5f, -0.5f, 2.5f };
+        var yPositions = new float[] { 0.5f, 3f };
+
+        var xPosition = xPositions[UnityEngine.Random.Range(0, xPositions.Length)];
+        var yPosition = yPositions[UnityEngine.Random.Range(0, yPositions.Length)];
+
+        m_TargetPosition = new Vector3(xPosition, yPosition, 0);
     }
 }
